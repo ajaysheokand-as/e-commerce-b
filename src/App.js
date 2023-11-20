03f3049869224129927504db4d009f95a43c04f0
Deepak7009 <deepak7009verma@gmail.com>
2023-11-20 19:37:04 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Product from './components/Product';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Product />
+    </>
+  );
+}
+
+export default App;
