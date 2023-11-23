b9376ff233933d57060257c929270d7456c2f3aa
educated jaat <royaljaat42000@gmail.com>
2023-11-23 16:33:23 +0530

    page add

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Product from './components/Product';
 import Navbar from './components/Navbar';
+import Fig2 from './components/fig2';
+
 
 function App() {
   return (
     <>
       <Navbar />
       <Product />
+      <Fig2/>
     </>
   );
 }
 
 export default App;
-dsdf
