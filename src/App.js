b152cd40295a7a17f2a6ae3fd255c255860f9efa
Bustedgut29 <gezansingh@gmail.com>
2023-12-07 16:10:33 +0530

    made responsive

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Product from './components/Product';
 import Navbar from './components/Navbar';
-import Section1 from './components/Section1/section1';
+import Banner from './components/Banner'
+import Fig2 from './components/fig2';
+import Section1 from './components/Section1';
+import Inquiry from './components/Inquiry';
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Product />
-      <Section1/>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <Banner />
+            <Product />
+            <Inquiry />
+            <Fig2 />
+            <Section1 />
+        </>
+    );
 }
 
 export default App;
